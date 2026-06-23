196629bbf99f5583289b98aaf007e7c52b0cbd5f
Holger Eiboeck <holger@eiboeck.de>
2026-06-23 22:09:49 +0200

    Update build environments

--- src/config.h
+++ src/config.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define DEBUG
+// DEBUG (serial logging) is set by the build env, not here:
+//   pio run -e d1_mini         -> release (silent, beeper self-test on boot)
+//   pio run -e d1_mini_debug   -> serial debug on 115200
 
 enum PinAssignments {
     encoderPinA = D5,
